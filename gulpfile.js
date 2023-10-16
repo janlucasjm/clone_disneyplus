5cb39bdcc462d7cf86b1668ce05af7f23c95beca
Jan Lucas <janlucas13@gmail.com>
2023-10-15 21:33:45 -0300

    finalizando hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function compilaSass() {
     return gulp.src('./src/styles/*.scss')
@@ -7,6 +8,14 @@ function compilaSass() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = function() {
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(compilaSass, images);
+
+exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(compilaSass));
 };
